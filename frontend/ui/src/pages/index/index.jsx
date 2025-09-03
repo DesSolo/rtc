5d6eb34c1a06039df41a62645b2f5bdaa092b780
Gennadiy Sadchikov <dessolo@mail.ru>
2025-09-03 18:47:52 +0300

    Init frontend

---
+++ frontend/ui/src/pages/index/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Index extends React.Component {
+    render() {
+        return (
+            <a href="https://google.com">google</a>                        
+        )
+    }
+
+}
+
+export default Index
