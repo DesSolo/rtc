4c06a606d385781ba1699ce8124d9e02755b9604
Gennadiy Sadchikov <dessolo@mail.ru>
2025-09-10 09:03:21 +0300

    fix: spell names

--- internal/server/health.go
+++ internal/server/health.go
@@ -2,8 +2,8 @@ package server
 
 import "net/http"
 
-func (s *Server) handleHealth(w http.ResponseWriter, r *http.Request) {
+func (s *Server) handleHealth(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"health": "ok"}`))
+	w.Write([]byte(`{"health": "ok"}`)) // nolint:errcheck
 }
